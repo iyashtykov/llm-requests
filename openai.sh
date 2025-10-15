eada9e5a85406ed1c36ebb8313f84641cc52c453
Igor Yashtykov <iyashtykov@users.noreply.github.com>
2025-10-15 10:05:32 +0100

    change openai model

--- openai.sh
+++ openai.sh
@@ -3,4 +3,4 @@
 SUB_PATH=/chat/completions
 LLM_URL=$OPENAI_BASE_URL$SUB_PATH
 
-curl -X POST $LLM_URL -H "Authorization: Bearer $OPENAI_API_KEY" -H "Content-Type: application/json" -d '{"model": "gpt-3.5-turbo", "messages": [{"role": "system", "content": "'"$CODIO_FREE_TEXT_ANSWER"'" }]}'
+curl -X POST $LLM_URL -H "Authorization: Bearer $OPENAI_API_KEY" -H "Content-Type: application/json" -d '{"model": "gpt-4o", "messages": [{"role": "system", "content": "'"$CODIO_FREE_TEXT_ANSWER"'" }]}'
