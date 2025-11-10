a698204db2178947ea4c6ae96208caf68208ca40
Igor Yashtykov <iyashtykov@users.noreply.github.com>
2025-11-10 16:49:49 +0000

    Updated anthropic model

--- anthropic.sh
+++ anthropic.sh
@@ -3,4 +3,4 @@
 SUB_PATH=/v1/messages
 LLM_URL=$ANTHROPIC_BASE_URL$SUB_PATH
 
-curl -X POST $LLM_URL -H "Authorization: Bearer $ANTHROPIC_API_KEY" -H "Content-Type: application/json" -H "anthropic-version: 2023-06-01" -d '{"max_tokens": 500, "model": "claude-3-5-sonnet-20240620", "messages": [ { "role": "user", "content": "'"$CODIO_FREE_TEXT_ANSWER"'" } ] }'
+curl -X POST $LLM_URL -H "Authorization: Bearer $ANTHROPIC_API_KEY" -H "Content-Type: application/json" -H "anthropic-version: 2023-06-01" -d '{"max_tokens": 500, "model": "claude-sonnet-4-5", "messages": [ { "role": "user", "content": "'"$CODIO_FREE_TEXT_ANSWER"'" } ] }'
